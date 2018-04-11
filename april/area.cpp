daa8e28e322ab894597ea29f14f59a2f333318bb
12345jatinmani <38291191+12345jatinmani@users.noreply.github.com>
2018-04-11 23:15:03 +0530

    area

--- april/area.cpp
+++ april/area.cpp
@@ -2,9 +2,29 @@
 #include<math.h>
 
 using namespace std;
-int area(int side)
+
+int area(int side )
+{
+  return side*side;
+}
+int area(int length,int bredth)
+{
+  return length*bredth;
+}
+double area(double radius)
+{
+  return 3.14*radius*radius;
+}
+double area(int a,int b,int c)
+{
+  double s=(a+b+c)/2;
+  return sqrt(s*(s-a)*(s`b)*(s-c));
+}
 int main()
 {
   cout<< area(41)<<endl;
+  cout<<area(12,5)<<endl;
+  cout<<area(1)<<endl;
+  cout<<area(1,2,3)<<endl;
   return 0;
 }
